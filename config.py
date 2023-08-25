7d5b08e8120896d7603a7aefeb72357447cdd881
Saykkuno (Ashutosh) <130449575+aashuxxD@users.noreply.github.com>
2023-08-25 17:19:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/aashuxxD/AnonXMusic-2.0",
+    "https://github.com/aashuxxD/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
